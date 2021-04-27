00f0a59de695da9116f33db676d0a90809ee404f
Jonah Karew <jonahkarew@gmail.com>
2021-04-27 17:17:24 -0400

    toggle added for light and dark modes

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wrapper from "../../components/Wrapper/Wrapper"
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 function Main() {
+
+    // states governing light and dark mode
+    const [mode, setMode] = useState("dark");
+    const [modeStatus, setModeStatus] = useState("Light Mode");
+
+
+
+    // toggles theme between light mode and dark mode
+    var toggleMode = () => {
+        if (mode === "dark"){
+            setMode("nothing");
+            setModeStatus("Dark Mode");
+        }
+        else {
+            setMode("dark");
+            setModeStatus("Light Mode");
+        }      
+    }
+
+
     return (
-        <Wrapper mode="dark">
+        <Wrapper mode={mode}>
             <Container>
                 <Row>
+                    <Col xs={2}><Button onClick={() => { toggleMode() }}>{modeStatus}</Button></Col>
                     <Col xs={12}>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Col>
                     <Col xs={3}>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Col>
                     <Col xs={3}>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Col>
