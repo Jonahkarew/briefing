3fc54d8a70ad964e68c6d0a639bcb0441d395c4c
Jonah Karew <jonahkarew@gmail.com>
2021-04-29 21:30:01 -0400

    update twitter list functions

--- src/components/TwitterFeed/TwitterFeed.js
+++ src/components/TwitterFeed/TwitterFeed.js
@@ -1,19 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap';
 
 
 function TwitterFeed() {
-    const [followList, setFollowList] = useState(["one thing", "two  thing"])
-    const call = () => {
-        console.log("everything is fine.")
-    }
+    const [followList, setFollowList] = useState(["Nothing here yet"]);
+    const [currentTwitterTerm, setCurrentTwitterTerm] = useState("");
+
+
+    // useEffect(() => {
+        
+    //     localStorage.getItem("twitterFeed")
+    //     if (TwitterFeed){
+    //         let tempList = JSON.parse(localStorage.getItem("twitterFeed"));
+    //         setFollowList(tempList)
+    //     }
+    //    else{
+    //        setFollowList(["POTUS"])
+    //    }
+    //   }, []);
+
+    const setLocalTwitterTerms = () => {
+        let tempList = JSON.parse(localStorage.getItem("twitterFeed"));
+        tempList.push(currentTwitterTerm);
+        setFollowList(tempList);
+        localStorage.setItem("twitterFeed", JSON.stringify(tempList));
+    };
+
+    const retrieveLocalTwitterTerms = () => {
+        let tempList = JSON.parse(localStorage.getItem("twitterFeed"));
+        setFollowList(tempList)
+    };
 
     return (
         <div>
-            <button onClick={call()}>click for tweets</button>
+            <InputGroup>
+                <Input onChange={(e)=>{setCurrentTwitterTerm(e.target.value)}} placeholder="Add to follow list." />
+                <InputGroupAddon addonType="append">
+                    <Button onClick={()=>{setLocalTwitterTerms()}} >
+                        Follow
+                    </Button>
+                </InputGroupAddon>
+            </InputGroup>
+            <button onClick={retrieveLocalTwitterTerms}>click for tweets</button>
             <br />
             <h2>List of tweets</h2>
             <ul>
-                {followList.map((follows)=>(
+                {followList.map((follows) => (
                     <li key={follows}>{follows}</li>
                 ))}
             </ul>
